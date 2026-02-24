35707dea7f635bfaa1a87e4551338976c1c8c5df
EXT.LORENZO.NAVARRO <lorenzonavarro262@gmail.com>
2026-02-23 20:54:12 -0500

    Configuración de nombres fijos aplicada

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Asegura que las rutas de los assets sean relativas
+  build: {
+    rollupOptions: {
+      output: {
+        // Configuramos nombres fijos sin el [hash] aleatorio
+        entryFileNames: `assets/index.js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/index.[ext]`
+      }
+    }
+  }
 })
